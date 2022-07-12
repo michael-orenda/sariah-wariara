576ccee1c41798b23bfb76ac0c088946f76f06e9
Gilbert Were <gilbert.were@stl-horizon.com>
2022-07-12 09:07:14 +0300

    Frontend Initial Commit

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,19 +1,6 @@
 <template>
   <v-footer dark padless>
     <v-card flat tile class="secondary white--text text-center">
-      <v-card-text>
-        <v-btn
-          v-for="(icon, i) in icons"
-          :key="i"
-          class="mx-4 white--text"
-          :href="icon.link"
-          target="_blank"
-          icon
-        >
-          <v-icon size="24px">{{ icon.text }}</v-icon>
-        </v-btn>
-      </v-card-text>
-
       <v-card-text class="white--text pt-0">
         Thank you, and may God bless you abundantly
       </v-card-text>
@@ -21,7 +8,8 @@
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Welcome to Baby Wariara's fundraising page</strong>
+        {{ new Date().getFullYear() }} —
+        <strong>Welcome to Baby Wariara's fundraising page</strong>
       </v-card-text>
     </v-card>
   </v-footer>
@@ -51,7 +39,7 @@ export default {
       },
       {
         text: "mdi-instagram",
-        link: "https://instagram.com/joabson_arley/",
+        link: "",
       },
     ],
   }),
