a8a4c0caab93cec8387e308c91de51a78f87740a
Gilbert Were <gilbert.were@stl-horizon.com>
2022-07-15 15:45:52 +0300

    Social Media Icons

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,19 @@
 <template>
   <v-footer dark padless class="mt-10">
     <v-card flat tile class="secondary white--text text-center">
+      <v-card-text>
+        <v-btn
+          v-for="(icon, i) in icons"
+          :key="i"
+          class="mx-4 white--text"
+          :href="icon.link"
+          target="_blank"
+          icon
+        >
+          <v-icon size="24px">{{ icon.text }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
       <v-card-text class="white--text pt-0">
         Thank you, and may God bless you abundantly
       </v-card-text>
@@ -8,7 +21,6 @@
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} —
         <strong>Welcome to Baby Wariara's fundraising page</strong>
       </v-card-text>
     </v-card>
